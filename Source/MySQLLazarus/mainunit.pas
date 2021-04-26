fd20b0a85e79271d963ae8f83bbadb69187c5228
Nitoich <bcmamka@gmail.com>
2021-04-26 20:17:33 +0300

    Сделал все практические по МДК1101

---
+++ Source/MySQLLazarus/mainunit.pas
@@ -0,0 +1,99 @@
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, mysql56conn, mysql57conn, SQLDB, DB, Forms, Controls,
+  Graphics, Dialogs, DBGrids, StdCtrls, ActnList;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    TablesList: TComboBox;
+    SaveButton: TButton;
+    DataSource: TDataSource;
+    DBGrid: TDBGrid;
+    MySQL57Connection: TMySQL57Connection;
+    SQLQuery: TSQLQuery;
+    SQLTransaction: TSQLTransaction;
+    procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure SaveButtonClick(Sender: TObject);
+    procedure TablesListChange(Sender: TObject);
+    procedure TablesListClick(Sender: TObject);
+    procedure TablesListKeyPress(Sender: TObject; var Key: char);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+procedure SetComponentCorrectPos();
+begin
+  MainForm.SaveButton.width:=round(MainForm.width / 10);
+  MainForm.SaveButton.top:=0;
+  MainForm.SaveButton.left:=MainForm.Width - MainForm.SaveButton.Width;
+  MainForm.DBGrid.top:=MainForm.TablesList.height; MainForm.DBGrid.Left:=1;
+  MainForm.DBGrid.width:=MainForm.width;
+  MainForm.DBGrid.height:=MainForm.height - MainForm.TablesList.Height;
+  MainForm.TablesList.Top:=1;
+  MainForm.TablesList.left:=1;
+  MainForm.TablesList.width:=MainForm.width - MainForm.SaveButton.width;
+end;
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  self.Width:=round(screen.Width / 2);
+  self.height:=round(screen.Height / 2);
+  SetComponentCorrectPos();
+end;
+
+procedure TMainForm.FormResize(Sender: TObject);
+begin
+  SetComponentCorrectPos();
+end;
+
+procedure TMainForm.SaveButtonClick(Sender: TObject);
+begin
+  SQLQuery.ApplyUpdates;
+  SQLTransaction.Commit;
+  SQLQuery.open;
+end;
+
+procedure TMainForm.TablesListChange(Sender: TObject);
+begin
+  SQLQuery.close;
+  case TablesList.ItemIndex of
+    0:SQLQuery.SQL.Text:='select * from book_fond';
+    1:SQLQuery.SQL.Text:='select * from catalog';
+    2:SQLQuery.SQL.Text:='select * from chitateli';
+    3:SQLQuery.SQL.Text:='select * from vidacha';
+  end;
+  SQLQuery.Open;
+end;
+
+procedure TMainForm.TablesListClick(Sender: TObject);
+begin
+
+end;
+
+procedure TMainForm.TablesListKeyPress(Sender: TObject; var Key: char);
+begin
+  Key:=#0;
+end;
+
+end.
+
---
+++ Source/MySQLZeosLazarus/mainunit.pas
@@ -0,0 +1,75 @@
+unit MainUnit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, DB, Forms, Controls, Graphics, Dialogs, DBGrids, StdCtrls,
+  ZConnection, ZDataset;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    TableList: TComboBox;
+    DataSource: TDataSource;
+    DBGrid1: TDBGrid;
+    ZConnection: TZConnection;
+    ZTable: TZTable;
+    procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure TableListChange(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+procedure CorrectComponentsPos();
+begin
+  MainForm.TableList.top:=0;
+  MainForm.TableList.Left:=0;
+  MainForm.TableList.width:=MainForm.Width;
+  MainForm.DBGrid1.Top:=MainForm.TableList.height;
+  MainForm.DBGrid1.left:=0;
+  MainForm.DBGrid1.width:=MainForm.width;
+  MainForm.DBGrid1.height:=MainForm.height - MainForm.TableList.height;
+end;
+
+{ TMainForm }
+
+procedure TMainForm.FormCreate(Sender: TObject);
+begin
+  self.width:=round(screen.width / 2);
+  self.height:=round(screen.height / 2);
+  CorrectComponentsPos();
+end;
+
+procedure TMainForm.FormResize(Sender: TObject);
+begin
+  CorrectComponentsPos();
+end;
+
+procedure TMainForm.TableListChange(Sender: TObject);
+begin
+  ZTable.close;
+  case TableList.ItemIndex of
+    0:ZTable.TableName:='book_fond';
+    1:ZTable.TableName:='catalog';
+    2:ZTable.TableName:='chitateli';
+    3:ZTable.TableName:='vidacha';
+  end;
+  ZTable.open;
+end;
+
+end.
+
